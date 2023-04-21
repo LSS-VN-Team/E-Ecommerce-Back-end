7df8321e82e80a1e946432f4a904d3c5db1bc184
Ngoc Hung <ngochungnguyen2000dhktyddn@gmail.com>
2023-04-21 11:45:26 +0700

    entitis

---
+++ src/modules/entities/statisticForYear.entity.ts
@@ -0,0 +1,37 @@
+import { Expose } from 'class-transformer';
+import { IsEmail, IsNumber, Min } from 'class-validator';
+import { BaseEntity } from 'libs/core/base/base-entity.entity';
+import { text } from 'stream/consumers';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import StatisticsOfDayEntity from './statisticsOfDay.entity';
+import StatisticsForMonthEntity from './statisticsForMonth.entity';
+
+@Entity()
+class StatisticsForYearEntity extends BaseEntity {
+  @Column({
+    type: 'simple-array',
+    
+  })
+  @Expose()
+  public statisticsForMonthList: StatisticsForMonthEntity[];
+
+  @Column({
+    type: 'simple-array',
+    
+  })
+  @Expose()
+  public productStatistcsList: object[];
+
+  @Column({
+    nullable: false, 
+    type:'float'
+  })
+  @Expose()
+  @IsNumber()
+  @Min(0)
+  public total: number;
+
+  
+}
+
+export default StatisticsForYearEntity;
